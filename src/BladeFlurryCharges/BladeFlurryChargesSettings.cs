b1a85b25e18c72651d50b8e59c42720a1b3d851b
anhlatuan91 <bloodykotex@gmail.com>
2018-09-24 15:37:45 +0700

    Add files via upload

---
+++ src/BladeFlurryCharges/BladeFlurryChargesSettings.cs
@@ -0,0 +1,16 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+
+namespace BladeFlurryCharges
+{
+    public class BladeFlurryChargesSettings : SettingsBase
+    {
+        public BladeFlurryChargesSettings()
+        {
+            LeftClick = false;
+        }
+        
+        [Menu("Use Left Click", 1)]
+        public ToggleNode LeftClick { get; set; }
+    }
+}
